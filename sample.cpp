bdd539bfcf2cc49ec8707b53193436e69ad82c85
Lisa Waddell <90416736+techteacher101nj@users.noreply.github.com>
2026-01-20 19:05:41 +0000

    inputtwo integers and add them, print them

--- sample.cpp
+++ sample.cpp
@@ -1,7 +1,10 @@
-// YOUR NAME HERE
+// Lisa Waddell
 #include <iostream>
 using namespace std;
 
 int main () {
-
+    int x, y;
+    cout <<"Enter two integers." << endl;
+    cin >> x >> y;
+    cout << "The sum is " << x+y << endl;
 }
